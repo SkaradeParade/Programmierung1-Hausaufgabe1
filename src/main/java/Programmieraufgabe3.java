76723dbeb60e296da71ea033fd084e6b2f03cf5c
Luca Riebieboldt <luca.der.simon@gmail.com>
2025-04-24 16:37:32 +0200

    Lösungen

--- src/main/java/Programmieraufgabe3.java
+++ src/main/java/Programmieraufgabe3.java
@@ -1,2 +1,15 @@
 public class Programmieraufgabe3 {
+    public static void main(String[] args) {
+        for(int linesprinted = 0; linesprinted < 4; linesprinted++) {
+            for(int charsprinted = 0; charsprinted <= 7; charsprinted++) {
+                if(linesprinted % 2 == 1){
+                    System.out.print(".");
+                }
+                else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
